0a2bd35c394e95c7cfc86101f39a22dca1d28e54
Surnmai <surnmai1@gmail.com>
2024-05-02 23:54:30 +0000

    Hero touched

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,11 @@ const Hero = () => {
   return (
     <>
       <section className="bg-hero3 h-[100vh] w-full bg-cover bg-no-repeat bg-center brightness-50">
-        <div className="container mx-auto"></div>
+        <div className="container mx-auto flex justify-center items-center">
+          <h3 className="text-8xl text-[#ff2eb2] font-bold z-50 translate-y-96 tracking-tight uppercase">
+            Beads & Button
+          </h3>
+        </div>
       </section>
     </>
   );
