c3e8ff2e69b9e91a76b8997e6665c6e65e1f94a0
Surnmai <surnmai1@gmail.com>
2024-05-03 01:15:13 +0000

    contact page done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,10 +10,27 @@ const Footer = () => {
     <>
       <footer className="py-5 bg-[#000] text-white">
         <div className="container mx-auto ">
-          <div className="md:flex md:items-center md:justify-between border-b bg-opacity-75 border-gray-500 pb-7">
+          <div className="md:flex  md:justify-between border-b bg-opacity-75 border-gray-500 pb-7">
+            {/* contact  */}
+            <div className="">
+              <div className="address">
+                <p className="font-extrabold text-xl capitalize">address: </p>
+                <p>P.O. Box Os 3251, Osabene-Koforidua</p>
+                <p>Mile 50 junction</p>
+              </div>
+              <div className="phone">
+                <p className="font-extrabold text-xl capitalize">phone: </p>
+                <p>(+233) 0 54 998 8953</p>
+              </div>
+              <div className="email">
+                <p className="font-extrabold text-xl capitalize">email: </p>
+                <p className="mail">komparisalome97@gmail.com</p>
+              </div>
+            </div>
+
             {/* index number  */}
-            <p className="flex gap-1 py-1">
-              <h3>Index number:</h3>
+            <p className="flex gap-2 py-1 font-extrabold text-xl">
+              <h3 className="">Index number:</h3>
               <span>4211290009</span>
             </p>
 
